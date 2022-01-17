ff83bccf0bf8860ee0b2e6f380ee45299c1f2dab
can <ttmcan@hotmail.com>
2022-01-18 02:07:22 +0300

    madlib game added

---
+++ PyPNo1_madlibs/madlibs.py
@@ -0,0 +1,17 @@
+# YoutuberName=input('please enter your youtuber name: ')
+
+# # 3 different methods there are to add a string and a variables
+# print('subscribe to ' +YoutuberName)
+# print('subscribe to {}'.format(YoutuberName))
+# print(f'subscribe to {YoutuberName}')
+
+
+adj= input('Adjective: ')
+verb1=input('Verb: ')
+verb2=input('Verb: ')
+famousPerson=input('Famous Person: ')
+
+madlib= f'Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famousPerson}!'
+
+print(madlib)
